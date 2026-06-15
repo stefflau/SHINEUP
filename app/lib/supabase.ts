de929f02cd508f5720df0c441734614398366a31
stefflau <87229839+stefflau@users.noreply.github.com>
2026-06-15 13:44:33 +0300

    fix supabase env vars

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://tcbagkreseztrjtqbfyd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjYmFna3Jlc2V6dHJqdHFiZnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NzQ3MzcsImV4cCI6MjA5NTI1MDczN30.kVbm-ch1Eq4T8SBb1BPruQXaEpH8sajb4Mfze1UAIbY"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
